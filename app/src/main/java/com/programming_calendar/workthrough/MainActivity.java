f85ed6472234b2f4d49275894ff28f30678257c1
Marcin Rybicki <m.rybicki1112@gmail.com>
2018-07-09 20:13:49 +0200

    Changed to current version of main.xml and the old version of MainActivity (static arrays).

--- app/src/main/java/com/programming_calendar/workthrough/MainActivity.java
+++ app/src/main/java/com/programming_calendar/workthrough/MainActivity.java
@@ -1,50 +1,120 @@
 package com.programming_calendar.workthrough;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.Button;
 import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
+import android.graphics.Color;
+import android.content.Context;
 
-import com.squareup.timessquare.CalendarPickerView;
+import java.util.List;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
+public class MainActivity extends Activity
+{
+    TextView txt1;
+    NextDate nextDate;
+    LinearLayout weekOneLayout, weekTwoLayout,
+            weekThreeLayout, weekFourLayout,
+            weekFiveLayout, weekSixLayout;
+    LinearLayout[] weeks;
+    Button[] days;
+    ProgHours progHours;
 
-public class MainActivity extends AppCompatActivity {
+    List<ProgHours> progHoursList;
+
+    public void onDayClick(View v, ColorBarDrawable colorBar){
+/*
+		String output = "";
+		int[] tagInfo = (int[])v.getTag();
+		for (int i = 0; i < 3; ++i){
+			if (i == 1) output += ((tagInfo[i] + 1) + " ");
+			else output += (tagInfo[i] + " ");
+		}
+		Toast.makeText(getApplicationContext(), output,Toast.LENGTH_SHORT).show();
+		*/
+        final HoursDialog hoursDialog = new HoursDialog(this);
+        v.setBackground(colorBar);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
 
-        Date today = new Date();
-        Calendar nextYear = Calendar.getInstance();
-        nextYear.add(Calendar.YEAR, 1);
+        txt1 = (TextView) findViewById(R.id.txt1);
 
-        CalendarPickerView datePicker = findViewById(R.id.calendar);
-        datePicker.init(today, nextYear.getTime())
-                .inMode(CalendarPickerView.SelectionMode.RANGE)
-                .withSelectedDate(today);
+        nextDate = new NextDate();
 
-        datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
-            @Override
-            public void onDateSelected(Date date) {
-                //String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+        ProgHoursDbHelper dbHelper = new ProgHoursDbHelper (this);
+        progHoursList = dbHelper.getAllProgHours();
 
-                Calendar calSelected = Calendar.getInstance();
-                calSelected.setTime(date);
+        progHours = progHoursList.get(2);
 
-                String selectedDate = "" + calSelected.get(Calendar.DAY_OF_MONTH)
-                        + " " + (calSelected.get(Calendar.MONTH) + 1)
-                        + " " + calSelected.get(Calendar.YEAR);
+        txt1.setText(progHours.getColor() + "");
 
-                Toast.makeText(MainActivity.this, selectedDate, Toast.LENGTH_SHORT).show();
+        weekOneLayout = (LinearLayout) findViewById(R.id.calendar_week_1);
+        weekTwoLayout = (LinearLayout) findViewById(R.id.calendar_week_2);
+        weekThreeLayout = (LinearLayout) findViewById(R.id.calendar_week_3);
+        weekFourLayout = (LinearLayout) findViewById(R.id.calendar_week_4);
+        weekFiveLayout = (LinearLayout) findViewById(R.id.calendar_week_5);
+        weekSixLayout = (LinearLayout) findViewById(R.id.calendar_week_6);
+
+        weeks = new LinearLayout[6];
+        days = new Button[6 * 7];
+
+        weeks[0] = weekOneLayout;
+        weeks[1] = weekTwoLayout;
+        weeks[2] = weekThreeLayout;
+        weeks[3] = weekFourLayout;
+        weeks[4] = weekFiveLayout;
+        weeks[5] = weekSixLayout;
+
+        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.FILL_PARENT);
+        buttonParams.weight = 1;
+
+        int[] colors = {Color.TRANSPARENT,0xff7070ff,0xffffdd66};
+        int[] hours = {7,2,3};
+        final ColorBarDrawable colorBar = new ColorBarDrawable(colors,hours);
+        int daysArrayCount = 0;
+
+        for (int weekNumber = 0; weekNumber < 6; ++weekNumber){
+            for (int dayInWeek = 0; dayInWeek < 7; ++dayInWeek){
+                final Button day = new Button(MainActivity.this);
+                day.setText(nextDate.getDayNumber());
+                day.setTextColor(nextDate.whatTextColor());
+                day.setBackgroundColor(nextDate.whatBackground());
+                day.setLayoutParams(buttonParams);
+                day.setSingleLine();
+
+                days[daysArrayCount] = day;
+                weeks[weekNumber].addView(day);
+
+                day.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v){
+                        onDayClick(v, colorBar);
+                    }
+                });
+
+                ++daysArrayCount;
+                nextDate.addOneDay();
             }
+        }
 
-            @Override
-            public void onDateUnselected(Date date) {
+				/*
+				int[] dateArr = new int[3];
+				dateArr[0] = dayNumber;
+				dateArr[1] = chosenMonth;
+				dateArr[2] = chosenYear;
+				days[i].setTag(dateArr);
+				*/
 
-            }
-        });
     }
 }
